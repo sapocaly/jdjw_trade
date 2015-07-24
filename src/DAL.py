5d9b281b077f594b96672bca288b7095c765f2b2
野 盛 <ye.sheng@centre.edu>
2015-07-25 01:01:04 +0800

    modified

--- src/DAL.py
+++ src/DAL.py
@@ -1,21 +1,24 @@
 
+# -*- coding: utf-8 -*- 
 import mysql.connector
+
 
 
 class StockDAL:
     def __init__(self):
+        #todo: 配置文件化
         self.conn = mysql.connector.connect(host='www.jdjw.org', user='jdjw', passwd='10041023', database='master_db')
-        self.cur = self.conn.cursor()
 
 
 
-    def insert_into(self,table_name, **args):
-        keys = args.keys()
-        values = [str(args[key]) for key in keys]
-
-        sql = "insert into " + table_name + " (" + ",".join(keys) + ")values(" + ",".join(values) + ")"
+    def insert_into(self,table_name, *args, **args_dict):
+        cursor = self.conn.cursor()
+        print args
+        keys = args_dict.keys()
+        values = ["'{0}'".format(args_dict[key]) if isinstance(str(args_dict[key]), basestring) else str(args_dict[key]) for key in keys]
+        sql = "insert into {0} ({1})values({2})".format(table_name, ",".join(keys), ",".join(values))
         print sql
-        self.cur.execute(sql)
+        cursor.execute(sql)
         self.conn.commit()
 
     def select_from(self, table_name, **args):
@@ -35,10 +38,10 @@ class StockDAL:
 
 
 
-
-a = StockDAL()
-a.insert_into('stock_list', ticker="'apple'")
-a.select_from('stock_list')
+if __name__ =='__main__':
+    a = StockDAL()
+    a.insert_into('stock_list', ticker="ali")
+    a.select_from('stock_list')
 
 
 

5772e24f1603cb3fef3656363d7bd6fb46564fc4
joker <billscchou@gmail.com>
2015-07-25 15:43:46 +0800

    updated clas and fetch_data

--- src/entry_classes.py
+++ src/entry_classes.py
@@ -5,47 +5,80 @@
 entries as classes
 """
 
+import urllib2
+import urllib
+import json
 import DAL
+import fetch_data
 
 
-class EntryMetaclass(type):
-    pass
+# governing class for all entries
+class Entry(dict):
+    def __init__(self, **kw):
+        for k in kw:
+            self[k] = kw[k]
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Entry' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
 
 
-class Stock(object):
-    __metaclass__ = EntryMetaclass
-
-    # todo:内存交互method
-    def __init__(self, ticker, name=None, exchange=None, pv_close=None, pv_volume=None):
-        self.ticker = ticker
-        self.name = name
-        self.exchange = exchange
-        self.pv_close = pv_close
-        self.pv_volume = pv_volume
-        self.dal_instance = None  # DAL 实例
+class Stock(Entry):
+    def __init__(self, **kw):
+        if 'ticker' not in kw:
+            raise Exception
+        super(Stock, self).__init__(**kw)
 
     def __str__(self):
         return 'Stock object (%s)' % self.ticker
     __repr__ = __str__
 
+    #
     def update(self):
-        pass
+        dal_instance = DAL.StockDAL()
+        kw = {}
+        for key in self.keys():
+            kw[key] = self[key]
+        dal_instance.update('stock_list', _id=self.id, **kw)
 
-    @staticmethod
-    def add(stock):
-        private_dal_instance = DAL.StockDAL()
-        private_dal_instance.insert_into('stock_list',
-                                         ticker=stock.ticker, name=stock.name,
-                                         exchange=stock.exchange, pv_close=stock.pv_close,
-                                         pv_volume=stock.pv_close)
+    def add(self):
+        dal_instance = DAL.StockDAL()
+        kw = {}
+        for key in self.keys():
+            kw[key] = self[key]
+        dal_instance.insert_into('stock_list', **kw)
 
+    def update_company_info(self):
+        # get company info and stores in db
+        # build query url for api
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = "select * from yahoo.finance.quote where symbol in ('"
+        yql_query = yql_query + self.ticker + "')"
+        yql_url = baseurl + urllib.urlencode({'q': yql_query}) +\
+            "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="
+        # get data
+        result = urllib2.urlopen(yql_url).read()
+        data = json.loads(result)
+        self['name'] = data['query']['results']['quote']['Name']
+        self['exchange'] = data['query']['results']['quote']['StockExchange']
+        self['pv_close'] = fetch_data.unicode2int(data['query']['results']['quote']['LastTradePriceOnly'])
+        self['pv_volume'] = data['query']['results']['quote']['Volume']
+        # update
+        self.update()
+
+    # 语句
     @staticmethod
     def get(**args):
         private_dal_instance = DAL.StockDAL()
         results = private_dal_instance.select_from('stock_list', **args)
         stocks = []
         for entry in results:
-            stock = Stock(entry[1], entry[2], entry[3], entry[4], entry[5])
+            stock = Stock(id=entry[0], ticker=entry[1], name=entry[2], exchange=entry[3], pv_close=entry[4], pv_volume=entry[5])
             stocks.append(stock)
         return stocks
 
@@ -54,43 +87,51 @@ class Stock(object):
         pass
 
 
-class Quote(object):
-    __metaclass__ = EntryMetaclass
-
-    def __init__(self, ticker, price, volume):
-        self.ticker = ticker
-        self.price = price
-        self.volume = volume
-        self.dal_instance = None
+class Quote(Entry):
+    def __init__(self, **kw):
+        super(Quote, self).__init__(**kw)
+        self.id = Stock.get(ticker=self.ticker)[0].id  # 从stock_list获取id
 
     def __str__(self):
         return 'Quote object (%s: $%s)' % (self.ticker, self.price)
     __repr__ = __str__
 
     def update(self):
-        pass
+        dal_instance = DAL.StockDAL()
+        kw = {}
+        for key in self.keys():
+            kw[key] = self[key]
+        dal_instance.update('stock_quote', _id=self.id, **kw)
+
+    def add(self):
+        dal_instance = DAL.StockDAL()
+        kw = {}
+        for key in self.keys():
+            if key != 'ticker':
+                kw[key] = self[key]
+        dal_instance.insert_into('stock_quote', **kw)
 
     @staticmethod
-    def add():
-        pass
-
-    @staticmethod
-    def get():
-        pass
+    def get(**args):
+        private_dal_instance = DAL.StockDAL()
+        results = private_dal_instance.select_from('stock_quote', **args)
+        quotes = []
+        for entry in results:
+            quote = Quote(id=entry[0], price=entry[1], volume=entry[2])
+            quotes.append(quote)
+        return quotes
 
     @staticmethod
     def rm():
         pass
 
 
-class Portfolio(object):
-    __metaclass__ = EntryMetaclass
-
+class Portfolio(Entry):
     def __init__(self):
         pass
 
     #def __str__(self):
-    #    return 'Portfolio object (%s)' % self.name
+    #    return 'Portfolio Entry (%s)' % self.name
     #__repr__ = __str__
 
     def update(self):
@@ -109,14 +150,12 @@ class Portfolio(object):
         pass
 
 
-class Transaction(object):
-    __metaclass__ = EntryMetaclass
-
+class Transaction(Entry):
     def __init__(self):
         pass
 
     #def __str__(self):
-    #    return 'Transaction object (%s)' % self.id
+    #    return 'Transaction Entry (%s)' % self.id
     #__repr__ = __str__
 
     def update(self):
@@ -135,14 +174,12 @@ class Transaction(object):
         pass
 
 
-class Indicator(object):
-    __metaclass__ = EntryMetaclass
-
+class Indicator(Entry):
     def __init__(self):
         pass
 
     #def __str__(self):
-    #    return 'Indicator object (%s)' % self.ticker
+    #    return 'Indicator Entry (%s)' % self.ticker
     #__repr__ = __str__
 
     def update(self):
@@ -161,6 +198,14 @@ class Indicator(object):
         pass
 
 if __name__ == '__main__':
-    #appl = Stock('APPL', 'Apple, Inc.', 'NYSE')
-    #Stock.add(appl)
-    Stock.get(ticker='AAPL')
+    #yoku = Stock(ticker='YOKU', name='Youku', exchange='NASDAQ')
+    #yoku.add()
+
+    #fb.name = 'Facebook'
+    #fb.pv_close = 12345
+    for s in Stock.get():
+        s.update_company_info()
+    #yoku.pv_close = '20.80'
+    #yoku.update()
+    #Stock.get(ticker='YOKU')
+    pass
--- src/fetch_data.py
+++ src/fetch_data.py
@@ -15,6 +15,17 @@ import DAL
 import entry_classes
 
 
+# unicode to integer (unit: cent)
+def unicode2int(unicode_str):
+    tmp = unicode_str.replace('.', '')
+    int_num = int(tmp)
+    return int_num
+
+
+def int2float(int_num):
+    pass
+
+
 # gets quotes from Yahoo Finance
 def fetch_quotes():
     # read from db
@@ -25,9 +36,9 @@ def fetch_quotes():
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = "select * from yahoo.finance.quote where symbol in ('"
     ticker_url = "','".join(tickers)
-    yql_query = yql_query + ticker_url
+    yql_query = yql_query + ticker_url + "')"
     yql_url = baseurl + urllib.urlencode({'q': yql_query}) +\
-        "')&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="
+        "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="
     #print yql_url
 
     # get data
@@ -35,13 +46,16 @@ def fetch_quotes():
     data = json.loads(result)
     quote_data = data['query']['results']['quote']
 
-    # todo: price unicode to int
-    quotes = [entry_classes.Quote(q['symbol'], q['LastTradePriceOnly'], q['Volume'])
+    quotes = [entry_classes.Quote(ticker=q['symbol'],
+                                  price=unicode2int(q['LastTradePriceOnly']),
+                                  volume=int(q['Volume']))
               for q in quote_data]
     print quotes
 
     # todo: write results into db
-
+    for q in quotes:
+        q.add()
 
 if __name__ == '__main__':
     fetch_quotes()
+    #print unicode2int('123.234')

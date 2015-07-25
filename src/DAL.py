8840d295e67e7f221a5ad30c589f6ab4b0c468e1
野 盛 <ye.sheng@centre.edu>
2015-07-25 11:23:24 +0800

    added update

--- src/DAL.py
+++ src/DAL.py
@@ -5,6 +5,10 @@ import mysql.connector
 
 
 class StockDAL:
+
+    ECHO = True
+    #todo:增加logger
+
     def __init__(self):
         #todo: 配置文件化
         self.conn = mysql.connector.connect(host='www.jdjw.org', user='jdjw', passwd='10041023', database='master_db')
@@ -12,35 +16,85 @@ class StockDAL:
 
 
     def insert_into(self,table_name,**args):
-        cursor = self.conn.cursor()
-        keys = args.keys()
-        values = ["'{0}'".format(args[key]) if isinstance(args[key], basestring) else (str(args[key]) if args[key] != None else 'null') for key in keys]
-        sql = "insert into {0} ({1})values({2})".format(table_name, ",".join(keys), ",".join(values))
-        print sql
-        cursor.execute(sql)
-        self.conn.commit()
+        try:
+            cursor = self.conn.cursor()
+            keys = args.keys()
+            values = ["'{0}'".format(args[key]) if isinstance(args[key], basestring) else (str(args[key]) if args[key] != None else 'null') for key in keys]
+            sql = "insert into {0} ({1})values({2})".format(table_name, ",".join(keys), ",".join(values))
+            if StockDAL.ECHO:
+                print sql
+            cursor.execute(sql)
+            self.conn.commit()
+        except Exception as e:
+            print e
 
     def select_from(self, table_name, **args):
-        if len(args) > 0:
-            sql = "select * from {0} where ".format(table_name)
-            for key in args:
-                sql += "{0} = {1} and ".format(key, "'{0}'".format(args[key]) if isinstance(args[key], basestring) else (str(args[key]) if args[key] != None else 'null'))
-            sql = sql[:-5]
-        else:
-            sql = "select * from " + table_name
-        print sql
+        try:
+            if len(args) > 0:
+                sql = "select * from {0} where ".format(table_name)
+                for key in args:
+                    sql += "{0} = {1} and ".format(key, "'{0}'".format(args[key]) if isinstance(args[key], basestring) else (str(args[key]) if args[key] != None else 'null'))
+                sql = sql[:-5]
+            else:
+                sql = "select * from " + table_name
+            if StockDAL.ECHO:
+                print sql
+            cur = self.conn.cursor()
+            cur.execute(sql)
+            toReturn = [i for i in cur]
+            if StockDAL.ECHO:
+                print toReturn
+            return toReturn
+        except Exception as e:
+            print e
+
+    def select(self, sql):
         cur = self.conn.cursor()
         cur.execute(sql)
         toReturn = [i for i in cur]
-        print toReturn
         return toReturn
+
+    def execute(self, sql):
+        cur = self.conn.cursor()
+        cur.execute(sql)
+        self.conn.commit()
+
+    def update(self, table_name, **args):
+        try:
+            cur = self.conn.cursor()
+            keys = args.keys()
+            query_keys = filter(lambda x: x[0] == '_',keys)
+            update_keys = filter(lambda x: x[0] != '_',keys)
+            query_keys = list(map(lambda x: x[1:], query_keys))
+            for key in keys:
+                args[key] = "'{0}'".format(args[key]) if isinstance(args[key], basestring) else (str(args[key]) if args[key] != None else 'null')
+            where_clause = ''
+            if len(query_keys) != 0:
+                where_clause = 'where '
+                for key in query_keys:
+                        where_clause += "{0} = {1} and ".format(key, args['_' + key])
+                where_clause = where_clause[:-5]
+            update_clause = ''
+            for key in update_keys:
+                update_clause += "{0} = {1}, ".format(key, args[key])
+            update_clause = update_clause[:-2]
+            print update_clause
+            sql = "update {0} set {1} {2}".format(table_name, update_clause, where_clause)
+            if StockDAL.ECHO:
+                print sql
+            cur.execute(sql)
+            self.conn.commit()
+        except Exception as e:
+            print e
+
 
 
 
 if __name__ =='__main__':
     a = StockDAL()
-    a.insert_into('stock_list', ticker="uber", name=None)
-    a.select_from('stock_list', ticker="ali")
+    #a.insert_into('stock_list', ticker="uber", name=None, age =3)
+    #a.select_from('stock_list', ticker="ali")
+    a.update('stock_list', _ticker="ali", ticker="alii", name="alibaba")
 
 
 

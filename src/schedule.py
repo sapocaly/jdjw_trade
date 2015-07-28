f67f5606728fe41033139a4df2ba8c9b6a0ef2f8
野 盛 <ye.sheng@centre.edu>
2015-07-28 23:27:41 +0800

    opt schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -5,14 +5,20 @@ import os
 import sched
 import datetime
 
+import logging
+import logging.config
+
 schedule = sched.scheduler(time.time, time.sleep)
 
+logging.config.fileConfig("../conf/jdjw_trade_logger.cfg")
+logger = logging.getLogger("jdjw_trade_fetch_digest")
 
 def perform_command():
     ##do something
+    logger.info('starting python')
     os.system("python fetch_data.py &")
     now =  datetime.datetime.now()
-    delta = (1000000 - now.microsecond) / 1000000.0
+    delta = (1100000 - now.microsecond) / 1000000.0
     schedule.enter(delta, 0, perform_command, ())
 
 

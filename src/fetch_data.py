331e70bc8a400c909cafd9129ff041691b1e2a00
野 盛 <ye.sheng@centre.edu>
2015-07-28 13:55:44 +0800

    added logger for fetch

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -12,9 +12,14 @@ import urllib2
 import urllib
 import json
 import datetime
+import logging
+import logging.config
 
-from DAL import StockDAL
 import db_models
+
+logging.config.fileConfig("../conf/jdjw_trade_logger.cfg")
+logger = logging.getLogger("jdjw_trade_fetch_digest")
+logger_alert = logging.getLogger("jdjw_trade_fetch_digest.alert")
 
 
 def db_time_format(string):
@@ -33,7 +38,6 @@ def chop_microseconds(time):
 
 
 def get_stock_list():
-
     # read from db
     results = db_models.Stock.search()
     ticker_id_dict = {}
@@ -44,35 +48,44 @@ def get_stock_list():
 
 # gets quotes from Yahoo Finance
 def fetch_quotes(ticker_id_dict):
-
+    global logger
+    global logger_alert
     # timestamp
     fetch_time = chop_microseconds(datetime.datetime.now())
+    start_time = datetime.datetime.now()
+    count = -1
+    try:
+        # build query url for api
+        baseurl = "https://query.yahooapis.com/v1/public/yql?"
+        yql_query = "select * from yahoo.finance.quote where symbol in ('"
+        ticker_url = "','".join(ticker_id_dict.keys())
+        yql_query = yql_query + ticker_url + "')"
+        yql_url = baseurl + urllib.urlencode({'q': yql_query}) + \
+                  "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="
+        # get data
+        result = urllib2.urlopen(yql_url).read()
+        data = json.loads(result)
+        quote_data = data['query']['results']['quote']
 
-    # build query url for api
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = "select * from yahoo.finance.quote where symbol in ('"
-    ticker_url = "','".join(ticker_id_dict.keys())
-    yql_query = yql_query + ticker_url + "')"
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) +\
-        "&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="
-
-    # get data
-    result = urllib2.urlopen(yql_url).read()
-    data = json.loads(result)
-    quote_data = data['query']['results']['quote']
-
-    # create objects
-    quotes = [db_models.Quote(id=ticker_id_dict[q['symbol']],
+        # create objects
+        quotes = [db_models.Quote(id=ticker_id_dict[q['symbol']],
                                   price=db_models.unicode2int(q['LastTradePriceOnly']),
                                   volume=q['Volume'],
                                   time=fetch_time)
-              for q in quote_data]
+                  for q in quote_data]
+        count = len(quotes)
+        # write results into db
+        db_models.Quote.add(quotes)
+        result = 'True'
+    except Exception as e:
+        result = 'False'
+        logger_alert.exception('fetch_error@' + str(fetch_time))
 
-    # write results into db
-    db_models.Quote.add(quotes)
+    finish_time = datetime.datetime.now()
+    log_string = "[({0}),({1}),{2},{3}]".format(start_time, finish_time, result, count)
+    logger.info(log_string)
 
 
 if __name__ == '__main__':
-    StockDAL.ECHO = False
     ticker_dict = get_stock_list()
     fetch_quotes(ticker_dict)
